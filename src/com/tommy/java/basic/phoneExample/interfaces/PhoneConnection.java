af4eb7f1589a44c0da838c812ba243a65ad39c16
Zhenya-Trikoz <60942325+Zhenya-Trikoz@users.noreply.github.com>
2023-01-13 15:30:51 +0200

    Delete src/com/tommy/java/basic/phoneExample directory

--- src/com/tommy/java/basic/phoneExample/interfaces/PhoneConnection.java
+++
@@ -1,6 +0,0 @@
-package com.tommy.java.basic.phoneExample.interfaces;
-
-public interface PhoneConnection {
-    void call(String phoneNumber);
-    void sendMessage(String phoneNumber,String textMessage);
-}
--- src/com/tommy/java/basic/phoneExample/interfaces/PhoneMedia.java
+++
@@ -1,7 +0,0 @@
-package com.tommy.java.basic.phoneExample.interfaces;
-
-public interface PhoneMedia {
-    void takePhoto();
-    void recordVideo();
-
-}
